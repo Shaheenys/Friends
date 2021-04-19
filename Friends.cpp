04419a32d274210c42582f534d753edfc5e86ea5
Shaheenys <82682144+Shaheenys@users.noreply.github.com>
2021-04-19 16:10:42 +0100

    Add files via upload

---
+++ Friends.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name;
+	string name2;
+
+	cout << "Please enter your name\n";
+	cin >> name;
+	cout << "please enter another name\n";
+	cin >> name2;
+	cout << name << " is now friends with " << name2 << " your welcome\n";
+	system("pause");
+}
